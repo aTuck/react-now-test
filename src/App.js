32b66b65352667b253cf073bef1911bf375f1cdd
Adam Tuck <tuckaet@gmail.com>
2019-06-06 15:57:17 -0700

    test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Changed this text for Now.js
           </p>
           <a
             className="App-link"
